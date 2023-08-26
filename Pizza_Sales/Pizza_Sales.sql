472ed0497a93003beb1ff751c74b473270467e73
Hitesh Choudhary <111753677+Hitesh9020@users.noreply.github.com>
2023-08-26 11:44:44 +0530

    Delete Pizza_Sales.sql

--- Pizza_Sales/Pizza_Sales.sql
+++
@@ -1,5 +0,0 @@
-select cast(cast(sum(quantity) as decimal(10,2)) / 
-cast(count(distinct order_id) as decimal(10,2)) as decimal(10,2)) 
-as Average_pizzas_per_order from pizza_sales
-
-select * from Pizza_sales;
